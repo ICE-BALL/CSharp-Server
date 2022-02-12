56840ffbd63e7a2b73959fa2f6340f8349ad991c
DESKTOP-2E2505B\오헌 <heon225@gmail.com>
2022-02-12 13:53:01 +0900

    Server
    Socket Program
    
    dummy client
    ServerCore 연동 기초

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DummyClient
 {
@@ -6,7 +9,42 @@ namespace DummyClient
     {
         static void Main(string[] args)
         {
-            
+            //DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            // 주소 가져오기
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            // 첫 번째로 알려준 주소
+            IPAddress ipAddr = ipHost.AddressList[0];
+            // 최종 주소  7777 => 문 느낌 클라이언트랑 맞춰 줘야함.
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 휴대폰 연결
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 문지기한테 입장 문의
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected to {socket.RemoteEndPoint.ToString()}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                int sendBytes = socket.Send(sendBuff);
+
+                // 받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                // 나간다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,67 +1,64 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class SpinLock
-    {
-        volatile static int _lock = 0;
-
-        public void Acquire()
-        {
-            while (true)
-            {
-                if (Interlocked.CompareExchange(ref _lock, 1, 0) == 0)
-                    break;
-
-                Thread.Yield();
-            }
-        }
-
-        public void Release()
-        {
-            _lock = 0;
-        }
-    }
-
     class Program
     {
-        static int num = 0;
-
-        static Mutex _lock = new Mutex();
-
-        static void Thread_1()
-        {
-            for (int i = 0; i < 10000; i++)
-            {
-                _lock.WaitOne();
-                num++;
-                _lock.ReleaseMutex();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < 10000; i++)
-            {
-                _lock.WaitOne();
-                num--;
-                _lock.ReleaseMutex();
-            }
-        }
-
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            //DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            // 주소 가져오기
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            // 첫 번째로 알려준 주소
+            IPAddress ipAddr = ipHost.AddressList[0];
+            // 최종 주소  7777 => 문 느낌 클라이언트랑 맞춰 줘야함.
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            t1.Start();
-            t2.Start();
+            // 문지기
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            Task.WaitAll(t1, t2);
+            try
+            {
+                // 문기지 교육
+                listenSocket.Bind(endPoint);
 
-            Console.WriteLine(num);
+                // 영업 시작
+                // backlog : 최대 대기수
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // 손님을 입장시킨다.
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // 받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcom to MMORPG Server !");
+                    clientSocket.Send(sendBuff);
+
+                    // 쫒아낸다
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
         }
     }
 }
